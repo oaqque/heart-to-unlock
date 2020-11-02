5d1f2c1984f8cae1e96f1192603cde421f0d121f
oaqque <oaqque@me.com>
2020-11-02 11:38:02 +1100

    Successfully sampling at 200Hz

--- data_streamer.py
+++ data_streamer.py
@@ -1,86 +1,59 @@
-# Python Libraries
-import requests
-import json 
-import os
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import random
 import time
-import argparse
 
-# For Token Generation
-from base64 import b64encode, b64decode
-from hashlib import sha256
-from urllib.parse import quote_plus, urlencode
-from hmac import HMAC
+# Using the Python Device SDK for IoT Hub:
+#   https://github.com/Azure/azure-iot-sdk-python
+# The sample connects to a device-specific MQTT endpoint on your IoT Hub.
+from azure.iot.device import IoTHubDeviceClient, Message
 
-# Import Configurations
-import config
+# The device connection string to authenticate the device with your IoT hub.
+# Using the Azure CLI:
+# az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
+CONNECTION_STRING = "HostName=COMP6733-Heart-Unlock.azure-devices.net;DeviceId=Sensortag;SharedAccessKey=SASxvyDdShBU4vNRGcNLo1jsofIJedTwIUeUgjdYwLY="
 
-# Azure IoT Hub
-URI = config.azure_uri
-KEY = config.azure_key
-IOT_DEVICE_ID = config.device_id
-POLICY = config.policy
+# Define the JSON message to send to IoT Hub.
+TEMPERATURE = 20.0
+HUMIDITY = 60
+MSG_TXT = '{{"temperature": {temperature},"humidity": {humidity}}}'
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Run Data Stream Server")
-    parser.add_argument('--sr', type=int, default=200, help="Sample Rate of Data Collection in Hertz")
-    return parser.parse_args()
+def iothub_client_init():
+    # Create an IoT Hub client
+    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+    return client
 
-def generate_sas_token(): 
-    """ Generates a Shared Access Signature (SAS) Token
-    
-    https://docs.microsoft.com/en-us/rest/api/eventhub/generate-sas-token
-    """
+def iothub_client_telemetry_sample_run():
 
-    expiry=3600
-    ttl = time.time() + expiry
-    sign_key = (quote_plus(URI) + '\n' + str(ttl)).encode('utf-8')
-    signature = b64encode(HMAC(b64decode(KEY), sign_key, sha256).digest())
+    try:
+        client = iothub_client_init()
+        print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
 
-    rawtoken = {
-        'sr' :  URI,
-        'sig': signature,
-        'se' : str(int(ttl))
-    }
+        while True:
+            # Build the message with simulated telemetry values.
+            temperature = TEMPERATURE + (random.random() * 15)
+            humidity = HUMIDITY + (random.random() * 20)
+            msg_txt_formatted = MSG_TXT.format(temperature=temperature, humidity=humidity)
+            message = Message(msg_txt_formatted)
 
-    rawtoken['skn'] = POLICY
+            # Add a custom application property to the message.
+            # An IoT hub can filter on these properties without access to the message body.
+            if temperature > 30:
+              message.custom_properties["temperatureAlert"] = "true"
+            else:
+              message.custom_properties["temperatureAlert"] = "false"
 
-    return 'SharedAccessSignature ' + urlencode(rawtoken)
+            # Send the message.
+            print( "Sending message: {}".format(message) )
+            client.send_message(message)
+            print ( "Message successfully sent" )
+            time.sleep(1)
 
-def send_message(token, message): 
-    """ Sends a message to IoT Azure Hub
+    except KeyboardInterrupt:
+        print ( "IoTHubClient sample stopped" )
 
-    token - Shared Access Signature (SAS) Token
-
-    message - json ready message to be sent 
-    """
-
-    url = 'https://{0}/devices/{1}/messages/events?api-version=2016-11-14'.format(URI, IOT_DEVICE_ID)
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": token
-    }
-    data = json.dumps(message)
-    print("\nSending Data:")
-    print(data)
-    response = requests.post(url, data=data, headers=headers)
-
-def read_from_sensortag():
-    #### TEMPORARY ####
-    data = 35
-    return data
-
-if __name__ == '__main__': 
-    # Parse arguments
-    args = parse_args()
-    sample_rate = args.sr
-    period = 1 / sample_rate
-
-    # Generate Shared Access Signatures (SAS) Token for Azure
-    token = generate_sas_token()
-
-    # Get data from Sensortag at 200Hz and send a message to Azure
-    while True: 
-        sens_data = read_from_sensortag() #### <- WRITE THIS FUNCTION ####
-        message = { "Acceleration": str(sens_data) }
-        send_message(token, message)
-        time.sleep(period)
+if __name__ == '__main__':
+    print ( "IoT Hub Quickstart #1 - Simulated device" )
+    print ( "Press Ctrl-C to exit" )
+    iothub_client_telemetry_sample_run()
---
+++ sensortag-sampler/udp-server.c
@@ -0,0 +1,144 @@
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+#include "contiki.h"
+#include "contiki-lib.h"
+#include "contiki-net.h"
+#include "sys/ctimer.h"	
+#include "board-peripherals.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#define DEBUG DEBUG_PRINT
+#include "net/ip/uip-debug.h"
+
+#define UIP_IP_BUF   ((struct uip_ip_hdr *)&uip_buf[UIP_LLH_LEN])
+
+#define MAX_PAYLOAD_LEN 120
+#define SAMPLE_RATE 500
+#define NUM_SAMPLES_SEND 20
+
+static struct uip_udp_conn *server_conn;
+static struct	ctimer sensor_timer;				
+static int value;
+static int countSamples;
+static int collected_samples[NUM_SAMPLES_SEND];
+
+PROCESS(udp_server_process, "UDP server process");
+AUTOSTART_PROCESSES(&resolv_process,&udp_server_process);
+/*---------------------------------------------------------------------------*/
+static void sensor_callback(void	*ptr)		{	
+  value = mpu_9250_sensor.value(MPU_9250_SENSOR_TYPE_GYRO_X);		
+  // printf("Value=%d.%02d\n\r", value/100, value%100);
+  int idx = countSamples % NUM_SAMPLES_SEND;
+  collected_samples[idx] = value;
+  countSamples++;
+  if (countSamples <= 600) { 
+    if (countSamples % NUM_SAMPLES_SEND == 0) {
+      PRINTF("SENDING DATA...\n");
+      uip_udp_packet_sendto(server_conn, collected_samples, sizeof(collected_samples), &server_conn->ripaddr, UIP_HTONS(3000));
+      memset(collected_samples, 0, NUM_SAMPLES_SEND);
+    }
+    ctimer_set(&sensor_timer, CLOCK_SECOND / SAMPLE_RATE, sensor_callback, NULL); // Callback timer for lux sensor
+  } else {
+    SENSORS_DEACTIVATE(mpu_9250_sensor);
+    PRINTF("All samples sent\n");
+  }
+}		
+/*---------------------------------------------------------------------------*/
+static void
+tcpip_handler(void)
+{
+  if(uip_newdata()) {
+    PRINTF("SensorTag Received Start Signal from \n");
+    PRINT6ADDR(&UIP_IP_BUF->srcipaddr);
+    PRINTF("\n");
+    uip_ipaddr_copy(&server_conn->ripaddr, &UIP_IP_BUF->srcipaddr);
+    PRINTF("Starting collection of data...\n");
+  }
+}
+/*---------------------------------------------------------------------------*/
+static void
+print_local_addresses(void)
+{
+  int i;
+  uint8_t state;
+
+  PRINTF("Server IPv6 addresses: ");
+  for(i = 0; i < UIP_DS6_ADDR_NB; i++) {
+    state = uip_ds6_if.addr_list[i].state;
+    if(uip_ds6_if.addr_list[i].isused &&
+       (state == ADDR_TENTATIVE || state == ADDR_PREFERRED)) {
+      PRINT6ADDR(&uip_ds6_if.addr_list[i].ipaddr);
+      PRINTF("\n\r");
+    }
+  }
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(udp_server_process, ev, data)
+{
+#if UIP_CONF_ROUTER
+  uip_ipaddr_t ipaddr;
+#endif /* UIP_CONF_ROUTER */
+
+  PROCESS_BEGIN();
+  PRINTF("UDP server started\n\r");
+
+#if RESOLV_CONF_SUPPORTS_MDNS
+  resolv_set_hostname("contiki-udp-server");
+#endif
+
+#if UIP_CONF_ROUTER
+  uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
+  uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
+  uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
+#endif /* UIP_CONF_ROUTER */
+
+  print_local_addresses();
+  //Create UDP socket and bind to port 3000
+  server_conn = udp_new(NULL, UIP_HTONS(0), NULL);
+  udp_bind(server_conn, UIP_HTONS(3001));
+  char buf[MAX_PAYLOAD_LEN];
+  while(1) {
+    PROCESS_YIELD();
+
+	  //Wait for tcipip event to occur
+    if(ev == tcpip_event) {
+      tcpip_handler();
+      SENSORS_ACTIVATE(mpu_9250_sensor);
+      ctimer_set(&sensor_timer, CLOCK_SECOND / SAMPLE_RATE, sensor_callback, NULL);	//Callback timer for lux sensor
+    } else if (ev == sensors_event && data == &mpu_9250_sensor) {
+    }
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/	
