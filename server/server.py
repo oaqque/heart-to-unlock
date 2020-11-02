87f62f3a7180444a4ac2f25e3ce56c8bf2ae388a
kakrial <kristian.fiebig@gmail.com>
2020-11-03 07:11:03 +1100

    Chnaged route

--- server/server.py
+++ server/server.py
@@ -3,15 +3,11 @@ import collector.collect_server as c
 # from .collector.collect_server import get_samples
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello world"
-
 @app.route('/data')
 def getData():
     return "[" + c.get_samples(600, 200).strip(",") +"]"
 
-@app.route('/welcome')
+@app.route('/')
 def welcome():
     return render_template('welcome.html')
 
